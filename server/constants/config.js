faad7b7685c3ca2f0c3c741cba4c8b68fbb9f691
Mayank Tripathi <mayankchamps27@gmail.com>
2024-06-24 15:58:09 +0530

    final push

--- server/constants/config.js
+++ server/constants/config.js
@@ -2,6 +2,7 @@ const corsOptions = {
   origin: [
     "http://localhost:5173",
     "http://localhost:4173",
+    "https://chat-app-cyan-seven-12.vercel.app/",
     process.env.CLIENT_URL,
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
